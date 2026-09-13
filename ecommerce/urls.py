7d45ec18730d24ec1eda1e0cb554081f8826ee11
Faith Chelangat <fchelangat53@gmail.com>
2026-09-13 06:58:19 +0300

    Edit home.html

--- ecommerce/urls.py
+++ ecommerce/urls.py
@@ -8,4 +8,6 @@ urlpatterns = [
     path('', include('storefront.urls')),
     path('cart/', include('cart.urls')),
     path('payment/', include('payment.urls')),
-]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
